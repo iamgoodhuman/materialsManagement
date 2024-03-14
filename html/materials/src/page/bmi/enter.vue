adbaed01897e66c8dca6dfe5e74a48dd6f000f50
zxt <552355101@qq.com>
2024-03-14 23:06:15 +0800

    添加bmi录入

--- html/materials/src/page/bmi/enter.vue
+++ html/materials/src/page/bmi/enter.vue
@@ -1,61 +1,62 @@
 <template>
-    <div class="enter">
-      <h3>体质录入</h3>
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="录入时间">
-          <el-input v-model="form.insure_company"></el-input>
-        </el-form-item>
-        <el-form-item label="保险名称">
-          <el-input v-model="form.insure_name"></el-input>
-        </el-form-item>
-  
-        <el-form-item label="保险类型">
-          <el-input v-model="form.insure_type"></el-input>
-        </el-form-item>
-  
-        <el-form-item label="保险日期">
-          <el-input v-model="form.insure_date"></el-input>
-        </el-form-item>
-  
-        <el-form-item label="被保险人">
-          <el-input v-model="form.insure_beneficiary"></el-input>
-        </el-form-item>
-  
-        <el-form-item>
-          <el-button type="primary" @click="onSubmit">录入</el-button>
-          <el-button>取消</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
-  </template>
-    
-  <script>
-  import axios from "@/utils/request";
-  export default {
-    name: "Home",
-    data() {
-      return {
-        form: {
-  
-        }
-      };
-    },
-    methods: {
-      onSubmit() {
-        axios.post('/demo/insure/enter', this.form).then((res) => {
-          console.log(res.data)
-          if (res.data.code == 200) {
-            this.$message({
-              message: res.data.msg,
-              type: 'success'
-            });
-          }
-        })
+  <div class="enter">
+    <h3>体质录入</h3>
+    <el-form ref="form" :model="form" :rules="rules"  label-width="80px">
+      <el-form-item label="录入时间" prop="time">
+        <el-date-picker v-model="form.time" type="date" value-format="yyyy-MM-dd HH:mm" placeholder="选择日期">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item label="体重(kg)" prop="weight">
+        <el-input v-model="form.weight"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit('form')">录入</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import axios from "@/utils/request";
+export default {
+  name: "Home",
+  data() {
+    return {
+      form: {
+        time: "",
+        weight: ""
+      },
+      rules: {
+        time: [
+          { required: true, message: '请选择日期', trigger: 'blur' },
+          { min: 0, max: 100, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        weight: [
+          { required: true, message: '请输入体重', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
       }
-  
-    },
-  };
-  </script>
-    
-  <style scoped></style>
-    
+    };
+  },
+  methods: {
+    onSubmit(formName) {
+      this.$refs[formName].validate((valid) => {
+
+      })
+      // axios.post('/demo/insure/enter', this.form).then((res) => {
+      //   console.log(res.data)
+      //   if (res.data.code == 200) {
+      //     this.$message({
+      //       message: res.data.msg,
+      //       type: 'success'
+      //     });
+      //   }
+      // })
+    }
+
+  },
+};
+</script>
+
+<style scoped></style>
--- html/materials/src/router/index.js
+++ html/materials/src/router/index.js
@@ -3,17 +3,17 @@ import VueRouter from 'vue-router'
 import Query from '../page/insure/query.vue'
 import Enter from '../page/insure/enter.vue'
 import Xbw from '../page/xbw/index.vue'
-// import BmiEnter from '../page/bmi/enter.vue'
+import BmiEnter from '../page/bmi/enter.vue'
 import Index from '../page/index.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
     routes: [
-
         {
             path: '/', component: Index, name: "Index", children: [
                 { path: '/insure/insure', component: Query },
                 { path: '/insure/enter', component: Enter },
+                { path: '/bmi/enter', component: BmiEnter },
             ]
         },
 
